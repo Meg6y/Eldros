aff42e770515012176f9698185c488bf09f5533d
Meg6y <87868692+Meg6y@users.noreply.github.com>
2024-08-09 19:52:16 +0200

    Quartz sync: Aug 9, 2024, 7:52 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian", "Technical shit/*"],
+    ignorePatterns: ["private", "templates", ".obsidian", "Technical shit/*", "#draft"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
